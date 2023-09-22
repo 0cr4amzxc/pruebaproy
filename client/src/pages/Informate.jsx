bdf3b7939e4b19cb16bac4399ba05eed829ce86c
Roy Ayala <troysg8203@gmail.com>
2023-09-22 10:41:18 -0400

    add client

---
+++ client/src/pages/Informate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Informate() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Informate</h1>
+    </div>
+  )
+}
+
+export default Informate
